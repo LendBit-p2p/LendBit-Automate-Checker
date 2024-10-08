b62bcf200ee68ddac74ce9df481d9d5368329539
PhantomOz <faniogor@gmail.com>
2024-10-08 02:11:00 +0100

    Initialized and add Interface

---
+++ src/interface/IProtocol.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+struct Request {
+    uint96 requestId;
+    address author;
+    uint256 amount;
+    uint16 interest;
+    uint256 totalRepayment;
+    uint256 returnDate;
+    address lender;
+    address loanRequestAddr;
+    Status status;
+}
+
+enum Status {
+    OPEN,
+    SERVICED,
+    CLOSED
+}
+
+interface IProtocol {
+    function getHealthFactor(address _user) external returns (uint8);
+    function getAllRequest() external view returns (Request[] memory);
+}
